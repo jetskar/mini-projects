d81913fe766405afb9e88b4fd01c696f8df46391
jetskar <harshsharmacode@gmail.com>
2024-04-10 23:53:18 +0530

    promise, fetch, async await

--- 09_advanced_one/promise.js
+++ 09_advanced_one/promise.js
@@ -73,19 +73,29 @@ async function consumePromiseFive() {
 }
 
 consumePromiseFive()
- 
+   
 // JSON api - https://jsonplaceholder.typicode.com/users 
 
-async function getAllUsers () {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        console.log(response);
-        const data = response.json();
-        console.log(data);
-    } catch(error) {
-        console.log('Error: ', error);
-    }
-}
+// async function getAllUsers () {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         // console.log(response);
+//         const data = await response.json();
+//         console.log(data);
+//     } catch(error) {
+//         console.log('Error: ', error);
+//     }
+// }
 
-getAllUsers()
+// getAllUsers()
+
+fetch('https://api.github.com/users/jetskar')
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
 
